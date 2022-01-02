88a55447a0d0f76f18d80650b0b64a6d130c98fc
Balkı <balki1985@gmail.com>
2022-01-02 11:46:22 +0300

    Init

--- setup.py
+++ setup.py
@@ -2,10 +2,11 @@ from setuptools import setup
 setup(name='data-science-utils',
       version='0.1',
       description='Data Science Utilities',
-      url='https://github.com/cbalkig/repo',
+      url='https://github.com/cbalkig/DataScienceUtils.git',
       author='C. Balkı Gemirter',
       author_email='cavide.balki@gmail.com',
       license='MIT',
       packages=['module'],
       install_requires=['statsmodels>=0.13.1',
-                        'matplotlib>=3.5.1'])
+                        'matplotlib>=3.5.1',
+                        'pandas>=1.3.5'])
